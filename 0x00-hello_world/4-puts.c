6a621663b7d9a487fa32f7c141e407b9679f7a89
Osoraa <sochi98.sn@gmail.com>
2022-04-11 01:48:55 -0700

    Solution to Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
+int main(void)
+{
+	char text[52] = "\"Programming is like building a multilingual puzzle";
+
+	puts(text);
+
+	return (0);
+}
