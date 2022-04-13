2a91723eb1fcc2c38a3fae39a41609d22cd0a93d
Osoraa <sochi98.sn@gmail.com>
2022-04-13 13:26:48 -0700

    Update requirements for mandatory 0x01-variables_if_else_while task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point - Prints possible combinations of double digits
+ *
+ * Return: 0 if succcessful, 1 otherwise
+ */
+int main(void)
+{
+	int i, ii, j, jj;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (ii = 48; ii <= 57; ii++)
+		{
+			for (j = i; j <= 57; j++)
+			{
+				for (jj = ii + 1; jj <= 57; jj++)
+				{
+					putchar(i);
+					putchar(ii);
+					putchar(32);
+					putchar(j);
+					putchar(jj);
+					if (i == 57 && ii == 56 && j == 57 && jj == 57)
+						break;
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
