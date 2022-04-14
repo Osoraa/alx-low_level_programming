d1e9e71d9d84639b0cceb6163abe6b46aa697587
Osoraa <sochi98.sn@gmail.com>
2022-04-13 19:30:44 -0700

    Update requirements for mandatory 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,19 @@
 /* HEaders for work in this workspace */
-#ifndef PUTCHAR
-#define PUTCHAR
+#ifndef PUTCHAR_H
+#define PUTCHAR_H
 
-	#include "_putchar.c"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 #endif
