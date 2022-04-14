b070664bbd90afe28b1276c11f3010c544b6b5d1
Osoraa <sochi98.sn@gmail.com>
2022-04-14 16:58:43 -0700

    Update requirements for mandatory 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - As the name implies oh.
+ * @n: number to compute last digit.
+ *
+ * Return: the last digit of n.
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
