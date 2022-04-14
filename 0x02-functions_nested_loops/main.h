e95bdad930bd0dcac54c001554a3153a861e0933
Osoraa <sochi98.sn@gmail.com>
2022-04-14 04:47:12 -0700

    Update requirements for mandatory 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,14 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include "_putchar.c"
+/**
+ * main - Function declarations.
+ * @args: values to be used in the functions.
+ *
+ * Return: int returns 1 if successful
+ * void returns none
+ * char may return the character it worked on, depends.
+ */
+int _putchar(char c);
 
 #endif	/* _MAIN_H_ */
