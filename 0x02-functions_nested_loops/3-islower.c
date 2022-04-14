8af780f852dc96edff9d1551dca77c6a30080844
Osoraa <sochi98.sn@gmail.com>
2022-04-14 15:46:15 -0700

    Update requirements for mandatory 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is lowercase.
+ *
+ * Return: 1 if lowercase; 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c < 'a' || c > 'z')
+		return (0);
+	return (1);
+}
