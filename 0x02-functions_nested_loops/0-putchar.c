08f4ed986fd937f970393edfeaad796de9e70eb4
Osoraa <sochi98.sn@gmail.com>
2022-04-13 17:44:58 -0700

    First task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.c"
+
+/**
+ * main - Program entry point - Prints the string "_putchar"
+ *
+ * Returns: 0 if successful, wouldn't compile otherwise
+ */
+int main(void)
+{
+	int i;
+	char str[9] = "_putchar";
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+	return (0);
+}
