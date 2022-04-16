f03ba36067eb4e88c966fbc400bb06e9d36d5176
Osoraa <sochi98.sn@gmail.com>
2022-04-16 05:50:06 -0700

    Update requirements for mandatory 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Print a square using the '#' char.
+ * @size: the size of the square.
+ *
+ * Return: None.
+ */
+void print_square(int size)
+{
+	int row = 0;
+
+	while (row < size)
+	{
+		int column = 0;
+
+		while (column < size)
+		{
+			_putchar('#');
+			column++;
+		}
+
+		_putchar('\n');
+		row++;
+	}
+}
