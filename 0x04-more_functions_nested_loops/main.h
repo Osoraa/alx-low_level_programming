42fda1e597047b1c1c82819bb87c0721a74110bf
Osoraa <sochi98.sn@gmail.com>
2022-04-17 16:36:54 -0700

    Update requirements for mandatory 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,6 +17,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-int fizzBuzz(int *num);
+int fizzBuzz(int num);
+void print_triangle(int size);
 
 #endif	/* _MAIN_H_ */
