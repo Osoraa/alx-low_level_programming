56789a0f38d1fbd4c67e31363159af29c4fd01c8
Osoraa <sochi98.sn@gmail.com>
2022-04-19 04:35:07 -0700

    fibonacci advanced task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-	long int num = 1, prev = 1, temp, sum;
+	long int num = 1, prev = 1, temp, sum = 0;
 
 	while (num < 4000000)
 	{
 		if (num % 2 == 0)
 			sum += num;
+
 		temp = num;
 		num += prev;
 		prev = temp;
