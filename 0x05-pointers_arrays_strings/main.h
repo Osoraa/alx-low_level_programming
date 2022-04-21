cc5bb81762456649babf3c44cefd6f97bd555039
Osoraa <sochi98.sn@gmail.com>
2022-04-20 17:57:17 -0700

    Update requirements for mandatory 0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
-#include <stdio.h>
-
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
+#include <stdio.h>
 
 /**
  * main - Check functions
@@ -13,5 +13,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
 
 #endif	/* _MAIN_H_ */
