841949b7592dc979dc9520333137ea79512e762b
Osoraa <sochi98.sn@gmail.com>
2022-04-22 05:29:41 -0700

    Update requirements for mandatory 0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,18 @@
  */
 void puts_half(char *str)
 {
-	int end = 0;
+	int end = 0;	/* Variable for iteration */
 
-	while (str[end] != '\0')
+	while (str[end] != '\0')	/* Find the length of the string */
 		end++;
 
-	if (end % 2)
-		end = (end + 1) / 2;
+	if (end % 2)			/* If the length is positive, the start of the half string, */
+		end = (end + 1) / 2;	/* should be shorter than when length is negative. */
 	else
 		end /= 2;
 
-	while (str[end] != '\0')
+	while (str[end] != '\0')		/* Print half string */
 		printf("%c", str[end++]);
 
-	putchar('\n');
+	putchar('\n');			/* Print new line */
 }
