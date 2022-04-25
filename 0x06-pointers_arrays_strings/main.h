cc312afb73ee7613e66874ff1d06b8f906ee2b74
Osoraa <sochi98.sn@gmail.com>
2022-04-25 06:05:33 -0700

    Update requirements for mandatory 0x06-more_pointers_arrays_strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,5 +17,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *string);
 char *cap_string(char *string);
+char *leet(char *string);
 
 #endif	/* _MAIN_H_ */
