668a5b31da5e03d843a0cca1097c428cecc8fbc1
Osoraa <sochi98.sn@gmail.com>
2022-04-26 02:00:47 -0700

    Initial commit

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Program entry point.
+ *
+ * Return: Always 0.
+ */
+int main(void);
+
+#endif	/* _MAIN_H_ */
