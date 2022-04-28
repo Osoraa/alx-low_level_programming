6d02b96c728a6b4ef7b3c2abed29266fa8cc1a0f
Osoraa <sochi98.sn@gmail.com>
2022-04-28 21:00:05 +0100

    Fourth try

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,20 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	for (; *s != '\0'; s++)
 	{
 		if (*s == c)
 			return (s);
-		s++;
 	}
 
 	return ('\0');
+	/* while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+
+		s++;
+	}
+
+	return (s); */
 }
