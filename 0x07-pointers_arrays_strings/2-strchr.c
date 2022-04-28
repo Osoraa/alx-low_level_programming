458891f6552a7002b329747a1c9842e9665a1f1c
Osoraa <sochi98.sn@gmail.com>
2022-04-28 21:04:17 +0100

    someone else's code

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,17 +6,17 @@
  * @s: Pointer to string to search.
  * @c: Character in s to locate.
  *
- * Return: Pointer
+ * Return: String 
  */
-char *_strchr(char *s, char c)
+char _strchr(char *s, char c)
 {
-	for (; *s != '\0'; s++)
+	/* for (; *s != '\0'; s++)
 	{
 		if (*s == c)
 			return (s);
 	}
 
-	return (s);
+	return (s); */
 	/* while (*s != '\0')
 	{
 		if (*s == c)
@@ -26,4 +26,16 @@ char *_strchr(char *s, char c)
 	}
 
 	return (s); */
+
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
