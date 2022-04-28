e7ec70ebc58b5746ce3d52835e34607dd4980e38
Osoraa <sochi98.sn@gmail.com>
2022-04-28 21:10:31 +0100

    Still someone else's shit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,16 @@
  *
  * Return: Pointer to first occurence of string.
  */
-char _strchr(char *s, char c)
+char *_strchr(char *s, char c)
 {
-	for (; *s != '\0'; s++)
+	/* for (; *s != '\0'; s++)
 	{
 		if (*s == c)
 			return (s);
 	}
 
 	return (s);
+
 	/* while (*s != '\0')
 	{
 		if (*s == c)
@@ -26,4 +27,16 @@ char _strchr(char *s, char c)
 	}
 
 	return (s); */
+
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
