dcd2f943975e3081e125b5f5b5fe93f3f7ec4806
Osoraa <sochi98.sn@gmail.com>
2022-05-01 21:15:34 +0100

    Final on Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * check_prime - Checks for prime numbers.
+ *
+ * @i: to check if divisible.
+ * @n: Number to validate.
+ *
+ * Return: 0 if divisible, 1 otherwise.
+ */
+int check_prime(int i, int n)
+{
+	/* Valid if i becomes greater than half of n */
+	if (i >= n / 2)
+		return (1);
+
+	if (n % i == 0)
+		return (0);
+
+	return (check_prime(i + 2, n));
+}
+
+/**
  * is_prime_number - Recursively validates prime numbers.
  * @n: Number to validate.
  *
@@ -8,8 +28,18 @@
  */
 int is_prime_number(int n)
 {
+	/* Base cases */
 	if (n <= 1)
 		return (0);
 
-	return (check_prime(n / 2, n));
+	/* Invalid if divisible by 2 but not 2. */
+	if (n % 2 == 0 )
+	{
+		if (n == 2)
+			return (1);
+
+		return (0);
+	}
+
+	return (check_prime(3, n));
 }
