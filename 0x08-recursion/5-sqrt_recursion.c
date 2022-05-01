d37b845bc7c58f3b88afccfb4835591061d01824
Osoraa <sochi98.sn@gmail.com>
2022-05-01 01:33:30 -0700

    Update requirements for mandatory 0x07-even_more_pointers_arrays_strings

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Finds the natural square rot of a number using recursion.
+ * @n: Value to compute it's square root.
+ *
+ * Return: The natural square root of n. Else, returns -1.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n / n - )
+		return (N);
+	else if (N * N > n)
+		return (-1);
+
+	N += 1;
+	return (_sqrt_recursion(n));
+}
