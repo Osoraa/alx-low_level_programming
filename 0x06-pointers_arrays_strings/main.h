3e34ec447fd40ba119fea4ea4fbf4ec004d7da4a
Osoraa <sochi98.sn@gmail.com>
2022-05-02 01:03:05 +0100

    Move 100-rot13.c from 0x07 to 0x06

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -18,5 +18,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *string);
 char *cap_string(char *string);
 char *leet(char *string);
+char *rot13(char *string);
 
 #endif	/* _MAIN_H_ */
