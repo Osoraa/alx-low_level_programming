b8ccdae676c755dbb731600a4d6355c81c5aa847
Osoraa <sochi98.sn@gmail.com>
2022-05-10 09:27:02 +0100

    Write a function that creates an array of integers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -15,5 +15,6 @@ int main(void);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 #endif /* _MAIN_H_ */
