9b547d0e35ba67317413fe056b562618a8e4bdc0
Osoraa <sochi98.sn@gmail.com>
2022-05-10 07:44:24 +0100

    Write a function that allocates memory for an array, using calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	r = malloc(size * nmemb);
+	r = calloc(size, nmemb);
 
 	if (r == NULL)
 		return (NULL);
