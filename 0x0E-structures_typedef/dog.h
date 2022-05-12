cfb995c598e6522e95c011c06290d0d66a565db7
Osoraa <sochi98.sn@gmail.com>
2022-05-12 11:52:48 +0100

    Initial commit along with task 0 - dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - A dog class
+ *
+ * @name: Name of the dog instance.
+ * @age: Age of the dog instance.
+ * @owner: Owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG */
