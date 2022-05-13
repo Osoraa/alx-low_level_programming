d85c2dc67bd5d0970ce6549dcf304b697989e416
Osoraa <sochi98.sn@gmail.com>
2022-05-13 14:23:52 +0100

    Task 2 - Print members of a dog struct.

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Age: (nil)");
 
-		if (d->name != NULL)
+		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
 			printf("Owner: (nil)");
