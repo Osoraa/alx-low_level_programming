780af412a5bc3beba748fdb7cb804bca1ce6dbcc
Osoraa <sochi98.sn@gmail.com>
2022-05-14 01:57:32 +0100

    Task 2 - Write a function that searches for an integer.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -15,5 +15,6 @@
 int main(void);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* _F_POINTERS_H_ */
