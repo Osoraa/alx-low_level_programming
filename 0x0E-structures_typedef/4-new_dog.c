a5bc72e83360083e883d68229da31b2b6dcb294a
Osoraa <sochi98.sn@gmail.com>
2022-05-19 09:22:46 +0100

    Committing after a while - task 4 structures typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,8 @@
 #include "dog.h"
+#include "main.h"
 
 /**
- * new_dog - Crestes a new instance of the dog.
+ * new_dog - Creates a new instance of the dog.
  *
  * @name: name of the new dog.
  * @age: New dog's age in human years.
@@ -9,7 +10,25 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t new_dog = {name, age, owner};
+	dog_t *new_dog = malloc(sizeof(dog_t));
+	char *dog_name = malloc(strlen(name));
+	char *dog_owner = malloc(strlen(owner));
+	
+	if (new_dog == NULL)
+		return NULL;
 
-	return (&new_dog);
+	if (dog_name == NULL)
+		return NULL;
+	
+	if (dog_owner == NULL)
+		return NULL;
+
+	strcpy(dog_name, name);
+	strcpy(dog_owner, owner);
+
+	new_dog->name = dog_name;
+	new_dog->age = age;
+	new_dog->owner = dog_owner;
+
+	return (new_dog);
 }
