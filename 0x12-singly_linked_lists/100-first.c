3847613458d8a02378e7dd7c197e6994e220bc07
Osoraa <sochi98.sn@gmail.com>
2022-05-24 22:45:53 +0100

    Task 5 - The Hare and the Tortoise - Run a function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * pre_main - Function to run before main runs.
+ *
+ * Return: None.
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
