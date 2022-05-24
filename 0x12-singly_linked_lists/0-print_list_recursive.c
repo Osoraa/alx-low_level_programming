a5948089e96cf8525793aca3b2af330cef3a478b
Osoraa <sochi98.sn@gmail.com>
2022-05-24 11:59:09 +0100

    Solved task 0 recursively

---
+++ 0x12-singly_linked_lists/0-print_list_recursive.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a list_t recursively.
+ * @h: list to print it's elements.
+ *
+ * Return: Number of nodes in the linked list.
+ */
+size_t print_list(const list_t *h)
+{
+	/* If NULL pointer to list_t is passed */
+	if (!h)
+		return (0);
+
+	/* If string is NULL */
+	if (!(h->str))
+	{
+		printf("[0] (nil)\n");
+		return (1 + print_list(h->next));
+	}
+
+	/* Normal case */
+	printf("[%d] %s\n", h->len, h->str);
+	return (1 + print_list(h->next));
+}
