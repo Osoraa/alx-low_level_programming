da033887b69d63a978a5942ce687eb698ffe2c93
Osoraa <sochi98.sn@gmail.com>
2022-05-24 19:22:27 +0100

    Task 3 - Add node at the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node - Inserts a new node at the beginning of a linked list recursively.
+ *
+ * @head: List beginning.
+ * @str: String to populate new node with.
+ *
+ * Return: Address of updated list or NULL on failure.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node;
+
+	/* Recursive case to find end of list */
+	if (*head)
+		return(add_node_end(&((*head)->next), str));
+
+	node = malloc(sizeof(list_t));
+	if (!node)
+		return (NULL);
+
+	/* Case whereby str is NULL */
+	node->str = str == NULL ? NULL : strdup(str);
+	node->len = str == NULL ? 0 : strlen(str);
+	node->next = NULL;
+
+	*head = node;	/* New node becomes end of list */
+
+	return (node);
+}
