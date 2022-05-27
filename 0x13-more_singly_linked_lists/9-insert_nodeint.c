83a78078cfac1d75feb2849a20ea0b07f8198536
Osoraa <sochi98.sn@gmail.com>
2022-05-27 20:25:42 +0100

    Task 9 - Insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node;
 
-	if (!(*head))
+	if (!(*head) || (int) idx < 0)
 		return (NULL);
 
 	if (idx)
