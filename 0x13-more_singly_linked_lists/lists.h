482ff65ef7e67ebc057cfc5a2c05d182faa14573
Osoraa <sochi98.sn@gmail.com>
2022-05-27 23:47:55 +0100

    Testing validity of access token

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -51,5 +51,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 listint_t *get_intnode_at_index(listint_t *head, size_t index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+listint_t *insert_intnode_at_index(listint_t *head, size_t idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *locate_and_delete(listint_t *head, size_t index);
 
 #endif /* _LISTS_H_ */
