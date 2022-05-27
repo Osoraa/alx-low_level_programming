e43cfecd6bcf4b6d5eac3a2c07fa5ec564a86581
Osoraa <sochi98.sn@gmail.com>
2022-05-27 20:25:42 +0100

    Task 9 - Insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -55,7 +55,8 @@ size_t listint_len(const listint_t *h)
  *
  * Return: Node's address or NULL on failure.
  */
-listint_t *insert_intnode_at_index(listint_t *head, listint_t *prev, size_t idx, int n)
+listint_t *insert_intnode_at_index(listint_t *head, listint_t *prev,
+ size_t idx, int n)
 {
 	listint_t *node;
 
