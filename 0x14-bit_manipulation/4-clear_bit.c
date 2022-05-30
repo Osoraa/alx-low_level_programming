f9ed39e817d6a97ff2444d0f7a588266810e8a8a
Osoraa <sochi98.sn@gmail.com>
2022-05-31 00:14:22 +0100

    Task 4 - 100, Clear bit at specified index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * set_bit - Sets the bit at a given index to 1.
+ * clear_bit - Clears the bit at a given index, i.e. Clears the bit.
  *
- * @n: Pointer to number to set it's index bit.
- * @index: Bit's position to set.
+ * @n: Pointer to number to clear it's indexed bit.
+ * @index: Bit position to clear.
  *
  * Return: 1 if successful, -1 otherwise.
  */
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(*n) * 8)
 		return (-1);
 
-	*n |= (1 << index);
+	*n &= ~(1 << index);
 
 	return (1);
 }
