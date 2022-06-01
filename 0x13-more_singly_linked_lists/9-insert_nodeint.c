242d855d702ecda48f4e79d63775342f91a033cc
Osoraa <sochi98.sn@gmail.com>
2022-06-01 22:23:57 +0100

    Task 9 - More Singly linked lists for posterity sake.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node;
 
-	if (!head || idx >= listint_len(*head) || (int) idx < 0)
+	if (idx >= listint_len(*head) || (int) idx < 0)
 		return (NULL);
 
 	if (idx != 0)
