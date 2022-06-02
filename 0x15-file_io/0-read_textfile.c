a53b582f97b8bbb2d964405d2f7c08702cadd7ee
Osoraa <sochi98.sn@gmail.com>
2022-06-02 02:42:41 +0100

    Initial commit/Task 0 - Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints to stdout.
+ *
+ * @filename: Name of file with contents to read.
+ * @letters: Number of letters to read and print to stdout.
+ *
+ * Return Actual number of letters it read and printed.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fildes;
+	char *buffer = malloc(letters + 1);
+	ssize_t len_rd;
+
+	if (!filename)
+		return (0);
+
+	fildes = open(filename, O_RDONLY);
+	if (fildes == -1)
+		return (0);
+
+	len_rd = read(fildes, buffer, letters);
+	if (len_rd == -1)
+		return (0);
+
+	printf("%s", buffer);
+	free(buffer);
+
+	return (len_rd);
+}
