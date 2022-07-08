40f77206c9b5e9023646619c145e7171699c6b02
Osoraa <sochi98.sn@gmail.com>
2022-07-08 01:17:01 +0100

    Task 1 - List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -11,7 +11,7 @@ size_t dlistint_len(const dlistint_t *h)
 	size_t len = 0;
 
 	if (!h)
-		return 0;
+		return (0);
 
 	while (h)
 	{
@@ -19,5 +19,5 @@ size_t dlistint_len(const dlistint_t *h)
 		h = h->next;
 	}
 
-	return len;
+	return (len);
 }
