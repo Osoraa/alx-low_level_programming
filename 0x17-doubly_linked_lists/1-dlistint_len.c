207d973bdde1ac9150ce4d2306a0ca612d52d9ff
Osoraa <sochi98.sn@gmail.com>
2022-07-08 03:19:29 +0100

    Task 5 - Get node at index

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,16 +8,8 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t len = 0;
-
 	if (!h)
 		return (0);
 
-	while (h)
-	{
-		len++;
-		h = h->next;
-	}
-
-	return (len);
+	return (1 + dlistint_len(h->next));
 }
