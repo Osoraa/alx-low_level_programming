e0af81039708d082de9896fcfbe7d9407a2b721d
Osoraa <sochi98.sn@gmail.com>
2022-07-28 21:21:57 +0100

    Task 0 - >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,11 +12,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t *array;
 
 	if (!size || size <= 0)
-		return NULL;
+		return (NULL);
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
-		return NULL;
+		return (NULL);
 
 	array = malloc(sizeof(hash_node_t) * size);
 	if (!array)
