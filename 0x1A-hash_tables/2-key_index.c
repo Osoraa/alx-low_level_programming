58729b7b2fd8af47431d80dcb3e21e154aacfe82
Osoraa <sochi98.sn@gmail.com>
2022-07-28 22:52:18 +0100

    Task 2 - key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,10 +2,10 @@
 
 /**
  * key_index - Computes the index of a key in a hashtable.
- * 
+ *
  * @key: Key with which to compute index.
  * @size: size of the array of the hash table.
- * 
+ *
  * Return: index of the key in the hashtable.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
