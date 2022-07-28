0ae76378e926e546df56b1bb9ac12c86fc7166d6
Osoraa <sochi98.sn@gmail.com>
2022-07-28 22:52:18 +0100

    Task 2 - key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,8 +10,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	/* if (!size || size < 0)
-		return (0); */
+	if (!size)
+		return (0);
 
 	return (hash_djb2(key) % size);
 }
