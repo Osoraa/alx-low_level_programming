8b39c3adfe7806436f95d7990583d90b29ef15fc
Osoraa <sochi98.sn@gmail.com>
2022-07-29 01:07:25 +0100

    Task 3 - >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	size_t index;
-	hash_node_t *node, *location;
+	hash_node_t *node;
 
 	if (!ht || !key || !value)
 		return (0);
